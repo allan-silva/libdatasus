7aac08a66af90748312f13851449dc9c7a70be0e
allan.silva <allan@allansilva.com.br>
2024-01-27 16:17:47 -0300

    Add javadoc for libdatasus-parquet-dbf. (#21)

--- libdatasus-parquet-dbf/src/main/java/br/gov/sus/opendata/dbf/parquet/ConvertTask.java
+++ libdatasus-parquet-dbf/src/main/java/br/gov/sus/opendata/dbf/parquet/ConvertTask.java
@@ -3,6 +3,9 @@ package br.gov.sus.opendata.dbf.parquet;
 import java.nio.file.Path;
 import java.util.Objects;
 
+/**
+ * {@link ConvertTask} provides configurations about DBC/DBF conversion process
+ */
 public class ConvertTask {
   private final Path input;
 
@@ -38,18 +41,34 @@ public class ConvertTask {
     return Objects.hash(input, output, schemaName);
   }
 
+  /**
+   * Input file or directory to be converted.
+   * @return path to resource.
+   */
   public Path getInput() {
     return input;
   }
 
+  /**
+   * Output file or directory containing the result .parquet file.
+   * @return path to resource.
+   */
   public Path getOutput() {
     return output;
   }
 
+  /**
+   * Schema name used to emit parquet message.
+   * @return message name for parquet file.
+   */
   public String getSchemaName() {
     return schemaName;
   }
 
+  /**
+   * When {@link ConvertTask#getInput()} is a directory, enabling this feature allows to combine multiple DBC/DFB file in one single .parquet file.
+   * @return flag indication for combine multiple DBC/DBF file in a single parquet file.
+   */
   public boolean combine() {
     return combine;
   }
@@ -63,35 +82,43 @@ public class ConvertTask {
 
     boolean combine = false;
 
-    boolean decompressDbc = true;
-
+    /**
+     * @see ConvertTask#getInput()
+     */
     public Builder input(Path input) {
       this.input = input;
       return this;
     }
 
+    /**
+     * @see ConvertTask#getOutput()
+     */
     public Builder output(Path output) {
       this.output = output;
       return this;
     }
 
+    /**
+     * @see ConvertTask#getSchemaName()
+     */
     public Builder schemaName(String schemaName) {
       this.schemaName = schemaName;
       return this;
     }
 
+    /**
+     * @see ConvertTask#combine()
+     */
     public Builder combine(boolean combine) {
       this.combine = combine;
       return this;
     }
 
+    /**
+     * @see ConvertTask#combine()
+     */
     public Builder combineFiles() {
       this.combine = true;
-      return this;
-    }
-
-    public Builder decompressDbc(boolean decompressDbc) {
-      this.decompressDbc = decompressDbc;
       return this;
     }
 
